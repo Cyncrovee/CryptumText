d291eb2a5211907f1bca47488be209fd511f1da4
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-07-04 21:16:53 +0100

    chore: change name of file list context menu item

--- src/util_menu.rs
+++ src/util_menu.rs
@@ -86,7 +86,10 @@ pub fn file_list_context_menu_model_item() -> MenuModel {
     let menu = Menu::new();
 
     menu.insert_item(0, &MenuItem::new(Some("New File"), Some("file.new_file")));
-    menu.insert_item(1, &MenuItem::new(Some("Delete"), Some("list.delete_item")));
+    menu.insert_item(
+        1,
+        &MenuItem::new(Some("Move to Trash"), Some("list.delete_item")),
+    );
 
     return menu.into();
 }
