7e2ca635648631b7b158711eeec1b5e28391c8c9
Cyncrovee <cyncrovee@noreply.codeberg.org>
2025-10-21 16:51:53 +0100

    fix: at long last, the file tree works

--- src/fs/folder.rs
+++ src/fs/folder.rs
@@ -1,8 +1,7 @@
-use std::path::PathBuf;
-
 use gtk4::{
     DirectoryList, Label, SignalListItemFactory, SingleSelection, TreeExpander, TreeListModel,
-    gio::{File, FileInfo},
+    TreeListRow,
+    gio::{File, FileInfo, FileType},
     prelude::ListItemExt,
 };
 use sourceview5::prelude::*;
@@ -10,22 +9,29 @@ use sourceview5::prelude::*;
 use crate::app::model::State;
 
 pub fn load_folder_view(state: &mut State) {
-    let path = state.current_folder_path.clone();
-    let file = File::for_path(&path);
-    let dir_list = DirectoryList::new(Some("standard::name"), Some(&file));
+    let dir_list = DirectoryList::new(
+        Some("standard::*"),
+        Some(&File::for_path(&state.current_folder_path)),
+    );
     let model = TreeListModel::new(dir_list, false, false, move |o| {
-        let dir_str = o.downcast_ref::<FileInfo>().unwrap().name();
-        let mut dir_path = PathBuf::new();
-        dir_path.push(&path);
-        dir_path.push(&dir_str);
-        if dir_path.is_dir() {
-            let dir_list_local = DirectoryList::new(None, Some(&File::for_path(dir_path)));
+        let file_info = o.downcast_ref::<FileInfo>().unwrap();
+        if file_info.file_type() == FileType::Directory {
+            let dir_list_local = DirectoryList::new(
+                Some("standard::*"),
+                Some(
+                    file_info
+                        .attribute_object("standard::file")
+                        .unwrap()
+                        .dynamic_cast_ref::<File>()
+                        .unwrap(),
+                ),
+            );
             Some(dir_list_local.into())
         } else {
             None
         }
     });
-    let selection = SingleSelection::new(Some(model.model()));
+    let selection = SingleSelection::new(Some(model.clone()));
     let factory = SignalListItemFactory::new();
     factory.connect_setup(move |_, list_item| {
         list_item.set_child(Some(
@@ -33,11 +39,16 @@ pub fn load_folder_view(state: &mut State) {
         ));
     });
     factory.connect_bind(move |_, list_item| {
-        let item = list_item.item().unwrap();
-        let file_info = item.downcast_ref::<FileInfo>().unwrap();
+        let row = list_item.item().and_downcast::<TreeListRow>().unwrap();
+        let file_info = row.item().and_downcast::<FileInfo>().unwrap();
         let tree = list_item.child().and_downcast::<TreeExpander>().unwrap();
-        tree.set_list_row(model.row(list_item.position()).as_ref());
-        tree.set_child(Some(&Label::new(Some(file_info.name().to_str().unwrap()))));
+        let label = tree.child().and_downcast::<Label>().unwrap();
+        if file_info.file_type() == FileType::Directory {
+            label.set_text(&format!("{}/", &file_info.display_name()));
+        } else {
+            label.set_text(&file_info.display_name());
+        }
+        tree.set_list_row(Some(&row));
     });
     state.file_view.set_model(Some(&selection));
     state.file_view.set_factory(Some(&factory));
